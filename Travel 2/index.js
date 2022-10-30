3925dc4087ab87db1bba32fe70862bf5bea93a47
Valentina <105123172+m0nes1@users.noreply.github.com>
2022-10-30 17:08:56 +0300

    Add files via upload

---
+++ Travel 2/index.js
@@ -0,0 +1,8 @@
+const button = button
+// document .querySelector ('button');
+
+button .onclick = function () {
+    let name = prompt('Как вас зовут');
+    alert('Привет ' + name + ', рады видеть вас! ');
+}
+
